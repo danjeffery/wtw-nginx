9e3d86889c9f52d6ec78d228074ad26ac80bcaed
Dan Jeffery <danjeffery@gmail.com>
2018-10-22 23:24:14 -0600

    Initial commit:Changes to Vagrantfile looping provider parametersAdding nginx deployment

---
+++ README.md
@@ -0,0 +1,62 @@
+# Vagrant up kubernetes cluster using Ubuntu 18.04 and kubeadm
+This will spin up a multi-node kubernetes cluster on Ubuntu 18.04.
+It uses https://github.com/cloudnativelabs/kube-router for the pod network.
+
+## Requirements:
+- vagrant + vagrant-sshfs
+- libvirt or virtualbox
+
+## Usage:
+```
+vagrant plugin install vagrant-sshfs
+vagrant up
+```
+A vagrant up with spin up a new cluster. Default behavior is 1 master and 4 nodes.
+
+Multiple master setup has not been tested.
+
+Add/remove nodes by adding entries to config/servers.yaml. You MUST have node in the hostname and use 10.4.2.x for it to work without modifications.
+
+CPU and memory can be modified by adding them to the config/servers.yaml (provider settings will be passed directly to the provider):
+```
+k8s-node1:
+  vm:
+    ip: 10.4.2.11
+  provider:
+    cpus: 4
+    memory: 2048
+```
+(Future update will add extra disk support)
+
+### Once your cluster is running you can login to k8s-master and run kubectl as root:
+```
+vagrant ssh k8s-master1
+sudo -i
+kubectl get nodes -o wide
+```
+
+### You can also copy the config to your base OS and connect from there.
+Install kubectl on base OS: https://kubernetes.io/docs/tasks/tools/install-kubectl/
+```
+mkdir -p $HOME/.kube
+vagrant ssh k8s-master1 -- 'sudo cat /etc/kubernetes/admin.conf' > $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl get nodes -o wide
+```
+
+### Useful kubernetes commands.
+Spin up single use fedora or busybox container for troubleshooting and testing.
+```
+kubectl get pods -o wide --sort-by="{.spec.nodeName}"
+kubectl run -it fedora --image=fedora --restart=Never -- bash
+kubectl run -it busybox --image=busybox --restart=Never -- sh
+kubectl delete pod fedora
+kubectl delete pod busybox
+```
+
+Check status of all pods (including system)
+```
+kubectl get pods --all-namespaces --include-uninitialized
+```
+
+For more kubectl commands, check out the [cheat sheet!](https://kubernetes.io/docs/reference/kubectl/cheatsheet/)
---
+++ config/bootstrap-kube.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# TODO: Support dynamic ip ranges so 10.4 doesnt need to be hard coded.
+LEAD_OCTETS=10.4
+SERVER_IP=$(ip -f inet -o addr show | grep ${LEAD_OCTETS} | awk '{split($4,a,"/");print a[1]}')
+apt install -y apt-transport-https
+
+# We are running on 18.04 (bionic) but there is not currently a kubernetes repo for it. Using xenial for now.
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt update
+
+# https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
+# Ensure docker is installed
+apt install -y docker.io jq
+# Install k8s packages. kubelet will fail to start on the nodes until the master is configured.
+apt install -y kubelet kubeadm kubectl kubernetes-cni || true
+echo "KUBELET_EXTRA_ARGS=--node-ip=${SERVER_IP}" > /etc/default/kubelet
+systemctl enable docker
+systemctl start docker
+
+hostnameMatches() {
+  hostname | grep $1 > /dev/null
+  return $?
+}
+
+# Prevent swap error messages from stopping the provisioning
+swapoff -a
+
+# TODO: Make work with multiple masters.
+if hostnameMatches master1; then
+  # TODO: dynamically pull k8s-master1 address instead of hard coded.
+  kubeadm init --apiserver-advertise-address=10.4.2.10 --pod-network-cidr=10.244.0.0/16
+  mkdir -p $HOME/.kube
+  cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  chown $(id -u):$(id -g) $HOME/.kube/config
+  # Install kube-router networking.
+  KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
+  # If you add nodes later you will need to get a new kubeadm token. Just run the following command on k8s-master1 before the vagrant up of a new node.
+  kubeadm token create --print-join-command > /vagrant/config/kube-join.sh
+fi
+
+if hostnameMatches node; then
+  while [ ! -f /vagrant/config/kube-join.sh ]; do
+    echo "Kubernetes master is not yet ready"
+    sleep 1
+  done
+  echo "Kubernetes master is ready. Proceeding to join the cluster."
+  sh /vagrant/config/kube-join.sh
+fi
